a766c46c95c09563afecfee2719ec9aac3da5891
chocoboy08 <chocoboy0508@gmail.com>
2022-02-26 23:35:11 +0900

    2일차 야간 연등(메인페이지 90% 완성)

--- D-day_home.js
+++ D-day_home.js
@@ -1,8 +1,38 @@
 let date_info = new Date();
-let year = date_info.getFullYear();
-let month = date_info.getMonth();
-let date = date_info.getDate();
+let td_year = date_info.getFullYear();
+let td_month = date_info.getMonth() + 1;
+let td_date = date_info.getDate();
 
-let today = year + "." + month + "." + date;
+let today = td_year + "." + td_month + "." + td_date;
 
 document.querySelector("#today_date").innerHTML = today;
+
+function getDate(){
+    let ip_year = document.getElementById("year").value;
+    let ip_month = document.getElementById("month").value;
+    let ip_date = document.getElementById("date").value;
+    
+    compareDate(ip_year, ip_month, ip_date);
+}
+
+function compareDate(y, m, d){
+    let endDate = new Date(y, m, d);
+    let stDate = new Date(td_year, td_month, td_date);
+    let remain = (endDate.getTime() - stDate.getTime()) / (1000*60*60*24);
+    
+    if(remain < 0){
+        alert("날짜를 잘못 입력하셨습니다.");
+    }
+    else{
+        if(remain <= 50){
+            alert(remain + ", One step closer!");
+        }
+        else if(remain <= 100){
+            alert(remain + ", Cheer up!");
+        }
+        else{
+            alert(remain + ", Too bad..");
+        }
+    }
+}
+document.getElementById("letsSee").addEventListener('click', getDate)
